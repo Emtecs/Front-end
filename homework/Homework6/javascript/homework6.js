c10f9ea0a2b82878d5e1e9216b1db26649dcde00
Karolis <karolissov@gmail.com>
2018-10-01 14:00:45 +0300

    Homework 6 update
    Added form with JavaScript

---
+++ homework/Homework6/javascript/homework6.js
@@ -0,0 +1,9 @@
+document.getElementById('rsvp').addEventListener('click', function () {
+    document.querySelector('#main_form').style.display = 'block';
+});
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('#main_form').style.display = 'none';
+});
+document.querySelector('.close1').addEventListener('click', function () {
+    document.querySelector('#main_form').style.display = 'none';
+});
