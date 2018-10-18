1f12cfca4fc3bb7a49d57dc4703de0736ee2df86
Karolis <karolissov@gmail.com>
2018-10-18 20:44:02 +0300

    Update to Homeworks
    Clean and new dir

--- homework/Homework11-pizzaWeb/js/map.js
+++ homework/Homework11-pizzaWeb/js/map.js
@@ -1,7 +1,8 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZW10ZWNzIiwiYSI6ImNqbmVmbXl6dzA3ZDMzbHBkdGlvODB4dnkifQ.B58VyP6B2p7VW4riDun6Kw';
-var map = new mapboxgl.Map({
-    container: 'map',
+var mymap = new mapboxgl.Map({
+    container: 'mymap',
     style: 'mapbox://styles/mapbox/streets-v10',
     center: [-74.50, 40.05], // starting position
     zoom: 11 // starting zoom
 });
+
